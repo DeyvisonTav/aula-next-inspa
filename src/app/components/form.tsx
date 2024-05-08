ebae28fc2a18d870bfa4ea175778e88aa7696784
DeyvisonTav <deyvisontav@gmail.com>
2024-05-07 21:04:09 -0300

    feat: create next project and form-register component

---
+++ src/app/components/form.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+
+export function FormComponent() {
+  return (
+    <div className="flex flex-col gap-8 px-14 pt-11">
+      <div className="flex flex-col items-start">
+        <div className="font-semibold text-3xl ">Welcome to </div>
+        <div className="font-extrabold text-5xl text-[#6358DC]">
+          Design School
+        </div>
+      </div>
+
+      <form className="flex flex-col gap-10 pt-36">
+        <input
+          className="border border-zinc-200 bg-zinc-100 p-6 rounded-lg w-full"
+          placeholder="Email"
+          type="text"
+        />
+
+        <input
+          className="border border-zinc-200 bg-zinc-100 p-6 rounded-lg w-full"
+          placeholder="Password"
+          type="password"
+        />
+
+        <button
+          className="text-white bg-[#6358DC] text-center py-6 rounded-lg
+                           transition-all duration-500 hover:bg-[#6358DC]/70"
+        >
+          Login
+        </button>
+      </form>
+
+      <div className="text-center pt-20">
+        <div>
+          Don’t have an account?{" "}
+          <Link className="text-[#6358DC]" href="/register">
+            Register
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
